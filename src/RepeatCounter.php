0e29e6f720285d514608fec6c10170cfab35cffc
alexdbrown <student@epicodus.com>
2015-08-14 10:20:52 -0700

    Write method to pass second spec test.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,9 +3,31 @@
  {
      function countRepeats ($word, $string)
      {
-         if ($word == $string) {
-            return $word;
-        }
+        // $string_of_words = explode($string);
+        $count = 1;
+
+        //  foreach ($string_of_words as $individual_word) {
+             if ($word == $string) {
+                 $count += ($string);
+             } else {
+                 return "No words in string match original word.";
+             }
+        //  }
+
+        return $count;
+
+        //
+        //  if ($word == $string) {
+        //     array_push($count, $string);
+        // }
+        //
+        // $result= 0
+        //
+        // foreach ($)
+        //
+        // $repeats = count(in_array($count));
+        //     return $result;
+
      }
  }
 
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -3,27 +3,27 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        function test_countRepeats_sameWord()
-        {
-            //Arrange
-            $test_RepeatCounter = new RepeatCounter;
-            $word = "dog";
-            $string = "dog";
+        // function test_countRepeats_sameWord()
+        // {
+        //     //Arrange
+        //     $test_RepeatCounter = new RepeatCounter;
+        //     $word = "dog";
+        //     $string = "dog";
+        //
+        //     //Act
+        //     $result = $test_RepeatCounter->countRepeats($word, $string);
+        //
+        //     //Assert
+        //     $this->assertEquals("dog", $result);
+        //
+        // }
 
-            //Act
-            $result = $test_RepeatCounter->countRepeats($word, $string);
-
-            //Assert
-            $this->assertEquals("dog", $result);
-
-        }
-
-        function test_countRepeats_sameWord()
+        function test_countRepeats_sameWordCount()
         {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
             $word = "water";
-            $string = 1;
+            $string = "water";
 
             //Act
             $result = $test_RepeatCounter->countRepeats($word, $string);
